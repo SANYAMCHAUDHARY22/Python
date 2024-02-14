08f1fd8eabf4ea5105fbb9977d4fe4a64c6129c9
SANYAMCHAUDHARY22 <500083508@stu.upes.ac.in>
2024-02-14 14:17:46 +0530

    bubble sort algo

---
+++ #bubble sort.py
@@ -0,0 +1,28 @@
+#bubble sort
+
+import random
+import time 
+
+def bubble_sort(arr):
+
+
+    for i in range(len(arr)):
+        for j in range(len(arr) - 1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+    return arr
+
+
+
+
+if __name__ == "__main__":
+    arr = [random.randint(0, 100) for i in range(10)]
+
+
+
+    print(arr)
+    print(bubble_sort(arr)) 
+    print(arr)  
+
+    arr = [random.randint(0, 100) for i in range(100)]
