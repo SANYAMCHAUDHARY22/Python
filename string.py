9f0b89dec36ff93967d79de78efdf9002ee6cd3c
SANYAMCHAUDHARY22 <500083508@stu.upes.ac.in>
2024-02-16 00:00:54 +0530

    update

---
+++ string.py
@@ -0,0 +1,10 @@
+#python strings
+#Strings are amongst the most popular types in Python. We can create them simply by enclosing characters in quotes. Python treats single quotes the same as double quotes. Creating strings is as simple as assigning a value to a variable. For example −
+
+var1 = 'Hello World!'
+var2 = "Python Programming"
+#Accessing Values in Strings
+#Python does not support a character type; these are treated as strings of length one, thus also considered a substring.
+#To access substrings, use the square brackets for slicing along with the index or indices to obtain your substring. For example −
+print ("var1[0]: ", var1[0])
+print ("var2[1:5]: ", var2[1:5])
