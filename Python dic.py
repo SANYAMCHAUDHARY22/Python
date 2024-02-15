d292e19a7ab76e83c74bcd896c398730f01ea233
SANYAMCHAUDHARY22 <500083508@stu.upes.ac.in>
2024-02-15 23:52:19 +0530

    update

---
+++ Python dic.py
@@ -0,0 +1,10 @@
+# Create a dictionary
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+# Print the dictionary
+print(thisdict)
+#This code creates a dictionary with three key-value pairs: "brand" with the value "Ford", "model" with the value "Mustang", and "year" with the value 1964. Then it prints the dictionary as output.
