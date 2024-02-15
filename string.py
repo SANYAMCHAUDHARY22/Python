51819807adb1502a8fe1e6a3311046b580eeb287
SANYAMCHAUDHARY22 <500083508@stu.upes.ac.in>
2024-02-16 00:01:47 +0530

    updated

--- string.py
+++ string.py
@@ -8,3 +8,19 @@ var2 = "Python Programming"
 #To access substrings, use the square brackets for slicing along with the index or indices to obtain your substring. For example −
 print ("var1[0]: ", var1[0])
 print ("var2[1:5]: ", var2[1:5])
+#Python String replace() Method
+#The method replace() returns a copy of the string in which the occurrences of old have been replaced with new, optionally restricting the number of replacements to max.
+#Syntax
+#Following is the syntax for replace() method −
+#str.replace(old, new[, max])
+#Parameters
+#old − This is old substring to be replaced.
+#new − This is new substring, which would replace old substring.
+#max − If this optional argument max is given, only the first count occurrences are replaced.
+#Return Value
+#This method returns a copy of the string with all occurrences of substring old replaced by new. If the optional argument max is given, only the first count occurrences are replaced.
+#Example
+#The following example shows the usage of replace() method.
+str = "this is string example....wow!!! this is really string"
+print (str.replace("is", "was"))
+print (str.replace("is", "was", 3))
