8e3a4e61f4558e8b7d39458dfe6f9871bc88fc65
SANYAMCHAUDHARY22 <500083508@stu.upes.ac.in>
2024-02-22 12:36:21 +0530

    updated

--- *.py
+++ *.py
@@ -4,3 +4,21 @@ print(*array)
 str="sanyam"
 print(*str)
 #output: s a n y a m
+# Path: %2A.py
+array=[1,2,3,4,5]
+print(array)
+#output: [1, 2, 3, 4, 5]
+print(*array)
+#output: 1 2 3 4 5
+# Path: %2A.py
+array=[1,2,3,4,5]
+print(*array,sep=",")
+#output: 1,2,3,4,5
+print(*array,sep=" ")
+#output: 1 2 3 4 5
+print(*array,sep="\n")
+#output: 1
+#        2
+#        3
+#        4
+#        5
